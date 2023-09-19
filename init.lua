8cb9dff4d3da26f81265aa52705f0eb20ec5a539
LukeOfficial <136903118+SecondLuke242014@users.noreply.github.com>
2023-09-19 10:57:01 +0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -11,3 +11,11 @@ minetest.register_node('advblocks:concrete_wall',{
     groups = {oddly_breakable_by_hand=2};
     tiles = {'concretewall.png'};
 })
+
+-- the bonus
+
+minetest.register_node('advblocks:smear',{
+        description = 'Smear';
+        groups = {oddly_breakable_by_hand=3}; 
+        tiles = {'smear.tiff'};
+})
